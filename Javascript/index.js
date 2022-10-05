50332ed8c4b9a930344f577e008adce61deca771
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-05 17:16:40 +0530

    JavaScript

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,3 +4,24 @@ var b=10; // This is a initialised variable
 a=2;
 console.log(a)
 console.log(b)
+
+var arthmethic=10;
+var arth=13;
+var dec=0.07;
+
+arthmethic++; //incrementing
+arth--;  //decrementing
+
+console.log(arth + arthmethic);
+
+var rem=13%2;
+
+var string = "iam rohit" + " This is concatenation example"
+console.log(string)
+
+string += "and hes s good guy"
+console.log(string)
+
+var name = "Rohit"
+var sentence ="My name is " + name + " This is a test"
+console.log(sentence);
